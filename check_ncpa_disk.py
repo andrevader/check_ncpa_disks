10fd3a12ad542a450ccba19b61db8c2d50eecf49
Andre Pinheiro Ribas <andrevader@gmail.com>
2019-08-21 12:43:25 -0300

    Version 2.0

--- check_ncpa_disk.py
+++ check_ncpa_disk.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # by Andre Ribas
+# 2.0 20/08/2019 - Excluindo discos que nao sao fixos
+# 1.3 19/08/2019 - Corrigido perf sem disco
 # 1.2 17/06/2019 - Corrigido unidades com erro valor
 # 1.1 06/05/2019 - Corrigido perf
 # 1.0 25/03/2019 - Correcao status OK e version
 # 0.0 28/12/2018
-ver=1.2
+ver=2.0
 import datetime
 import sys
 import json
@@ -27,9 +29,9 @@ def main():
     parser.add_option("-x", "--exclude", dest="exclude",
                       help="Unidades Excluidas")
     parser.add_option("-w", "--warning", dest="warning",
-                      help="Informacoes Warning")
+                      help="Informacoes adicionais")
     parser.add_option("-c", "--critical", dest="critical",
-                      help="Informacoes Critical")
+                      help="Informacoes adicionais")
     (options, args) = parser.parse_args()
 
     disk_all( server=options.srvip, chave=options.keyncpa, porta=options.port, sop=options.versionso, exclude=options.exclude, warn=int(options.warning), crit=int(options.critical) )
@@ -53,7 +55,7 @@ def convbit(size):
             return res
     
 def disk_all( server, chave, porta, sop, exclude, warn, crit ):
-	command = "/usr/local/nagios/libexec/check_ncpa.py -H %s -t %s -P %s -M 'disk/logical/' -l" %(server, chave, porta)
+	command = "/usr/local/nagios/libexec/check_ncpa.py -H %s -t %s -P %s -M 'disk/logical' -l" %(server, chave, porta)
 	valor = subprocess.check_output(command, shell=True)
 	item_dict = json.loads(valor)
 	saida=0
@@ -69,12 +71,19 @@ def disk_all( server, chave, porta, sop, exclude, warn, crit ):
 			elif sop == 'linux' :
 				unid = a.replace('|','/')
 			unidtsts = b['used_percent'][0]
+			tunid = b['opts']
 			try:
 				vlrdisk = int(unidtsts)
 			except:
 				vlrdisk = 0
 			unidex = exclude.find(unid)
-			if unidex < 0 and unid != 'C:' and unid != '/':
+			if sop == 'windows' and tunid == 'rw,fixed':
+				valid=0
+			elif sop == 'linux' :
+				valid=0
+			else:
+				valid=1
+			if unidex < 0 and unid != 'C:' and unid != '/' and valid == 0:
 				if vlrdisk > crit :
 					saida=2
 					status="CRITICAL"
@@ -97,6 +106,7 @@ def disk_all( server, chave, porta, sop, exclude, warn, crit ):
 		mens2 = unidades
 	if len(mens2) < 1 :
 		mens2 = "Sem discos adicionais"
+		perf = "disk=0;0;0"
 	mens = "%s - %s\nversao=%s |%s" %(status, mens2, ver, perf)
 	print mens
 	sys.exit(saida)
